67cb1478080c73113ffe0fd52bdf90dea3032b4c
Randall Reynolds <randall.weylin.reynolds@gmail.com>
2023-07-24 16:01:03 -0400

    added ntrip client

---
+++ ntrip_client.py
@@ -0,0 +1,36 @@
+from gnss_corrections import ntrip_corrections
+
+import time
+import yaml
+
+def main():
+
+    settings = None
+
+    with open('settings.yaml','r') as file:
+        settings = yaml.safe_load(file)
+        print(settings)
+
+    if settings is None: 
+        print('error reading settings file')
+        return
+    
+    gnss_cors = ntrip_corrections(settings['corrections']['connection']['address'],
+                                  settings['corrections']['connection']['mountpoint'],
+                                  settings['corrections']['connection']['user'],
+                                  settings['corrections']['connection']['password'],
+                                  settings['corrections']['connection']['port'],
+                                  org='EMSG')
+    gnss_cors.start()
+    time.sleep(1)
+
+    t0 = time.time()
+
+    while (time.time() - t0) < 2:
+        time.sleep(1)
+
+    gnss_cors.stop()
+
+
+if __name__ == '__main__':
+    main()
